e98490b13d0849188f1ffed398ddf3f177ac7e69
Hiroki Takeda <takeda.hiroki.tech@gmail.com>
2026-09-15 07:39:34 +0000

    feat: initial commit

---
+++ .devcontainer/scripts/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Update System Packages.
+sudo apt-get update
+
+# Install gnupg2 for sharing Git credentials.
+# cf. https://code.visualstudio.com/remote/advancedcontainers/sharing-git-credentials#_sharing-gpg-keys
+sudo apt-get install -y gnupg2
+
+# Set the owner of the directories mounted via the volumes.
+sudo chown vscode:vscode node_modules
+sudo chown vscode:vscode "$HOME/.claude"
+
+# Install npm dependencies.
+npm install -g npm
+npm install
+
+# Set aliases
+echo "alias ll=\"ls -l\"" >> "$HOME/.bash_aliases"
---
+++ docs/README.md
@@ -0,0 +1,5 @@
+# Tech Blog
+
+---
+
+このサイトは [スマートフォントUI](https://flopdesign.com/freefont/smartfont.html) を使用しています。フォントは [IPAフォントライセンスv1.0](https://moji.or.jp/ipafont/license/) のもとで提供されています。
